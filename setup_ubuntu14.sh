91266cdf0dc6fde807989d463aab11aea6774dc8
Steve Ash <steve.ash@argodata.com>
2015-06-10 16:20:21 -0500

    adding sudo back in

--- setup_ubuntu14.sh
+++ setup_ubuntu14.sh
@@ -2,12 +2,12 @@
 ## initial install script that just sets up the base tools should be run as root
 ##
 
-apt-get update -y
-apt-get install -y wget git zsh curl htop byobu tmux emacs nano net-tools telnet zip unzip sharutils unrar p7zip-full rar
+sudo apt-get update -y
+sudo apt-get install -y wget git zsh curl htop byobu tmux emacs nano net-tools telnet zip unzip sharutils unrar p7zip-full rar
 
 echo installing java
-add-apt-repository -y ppa:webupd8team/java
-apt-get update
-apt-get install -y oracle-java8-installer oracle-java8-set-default
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install -y oracle-java8-installer oracle-java8-set-default
 
 echo '*************** ALL DONE ******************'
