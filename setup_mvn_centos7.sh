35d0aa3cb19aa4275200c1458016759f1fb596e1
Steve Ash <stevemash@gmail.com>
2015-06-17 21:30:07 -0500

    updating java script

--- setup_mvn_centos7.sh
+++ setup_mvn_centos7.sh
@@ -3,3 +3,9 @@
 sudo yum install -y maven
 
 curl -s get.gvmtool.net | bash
+sed -i 's/gvm_auto_answer=.*/gvm_auto_answer=true/' ~/.gvm/etc/config 
+gvm install groovy
+
+echo '********************************************'
+echo ' ALL DONE'
+echo '********************************************'
